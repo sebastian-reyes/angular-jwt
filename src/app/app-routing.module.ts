9ebe6354092023a2f06ebc8d4f1b014a9b596570
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2021-09-13 20:20:42 -0500

    Creando primeros componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientesComponent } from './pages/home/clientes/clientes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'clientes', component: ClientesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientesComponent } from './pages/home/clientes/clientes.component'
+import { HeaderComponent } from './pages/home/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientesComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
