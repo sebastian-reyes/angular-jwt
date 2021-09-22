04896c956f6aa5c9353493272f0b608cead93a2a
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2021-09-21 19:39:06 -0500

    Manejando acceso denegado error 403

--- src/app/pages/home/clientes/clientes.component.ts
+++ src/app/pages/home/clientes/clientes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from 'src/app/model/cliente';
+import { AuthService } from 'src/app/service/auth.service';
 import { ClienteService } from 'src/app/service/cliente.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -11,7 +13,7 @@ export class ClientesComponent implements OnInit {
 
   clientes: Cliente[];
 
-  constructor(private clienteService: ClienteService) { }
+  constructor(private clienteService: ClienteService, public authService: AuthService) { }
 
   ngOnInit(): void {
     this.clienteService.getClientes().subscribe(
@@ -19,6 +21,29 @@ export class ClientesComponent implements OnInit {
         this.clientes = clientes
       }
     );
+  }
+
+  eliminar(cliente: Cliente): void {
+    Swal.fire({
+      title: 'Estás seguro?',
+      text: "Está seguro de eliminar este usuario?",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, elimínalo!',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.clienteService.eliminarCliente(cliente.usuario_id).subscribe();
+        console.log(cliente.usuario_id);
+        Swal.fire(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        )
+      }
+    })
   }
 
 }
--- src/app/service/cliente.service.ts
+++ src/app/service/cliente.service.ts
@@ -4,26 +4,43 @@ import { Cliente } from '../model/cliente';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, map } from 'rxjs/operators'
 import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
+  constructor(private http: HttpClient, private router: Router, private authService: AuthService) { }
+
   private urlBase = "http://localhost:9898/api/usuarios";
 
   private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+  private agregarAuthorizationHeader() {
+    let token = this.authService.token;
+    if (token != null) {
+      return this.httpHeaders.append('Authorization', 'Bearer ' + token)
+    }
+    return this.httpHeaders;
+  }
 
   private noAutorizado(e): boolean {
-    if (e.status == 401 || e.status == 403) {
+    if (e.status == 401) {
       this.router.navigate(['/login']);
+      return true;
+    } else if (e.status = 403) {
+      Swal.fire(
+        'Acceso denegado',
+        'Usted no cuenta con permisos para acceder a este recurso',
+        'error'
+      );
+      this.router.navigate(['/clientes']);
       return true;
     } else {
       return false;
     }
   }
-
-  constructor(private http: HttpClient, private router: Router) { }
 
   getClientes(): Observable<Cliente[]> {
     return this.http
@@ -32,7 +49,7 @@ export class ClienteService {
   }
 
   getCliente(id): Observable<Cliente> {
-    return this.http.get<Cliente>(`${this.urlBase}/${id}`)
+    return this.http.get<Cliente>(`${this.urlBase}/${id}`, { headers: this.agregarAuthorizationHeader() })
       .pipe(
         catchError(e => {
           if (this.noAutorizado(e)) {
@@ -55,7 +72,24 @@ export class ClienteService {
           if (e.status == 400) {
             return throwError(e);
           }
-          console.error
+          console.error(e);
+        })
+      );
+  }
+
+  eliminarCliente(id: number): Observable<Cliente> {
+    return this.http.delete<Cliente>(`${this.urlBase}/eliminar/${id}`, { headers: this.agregarAuthorizationHeader() })
+      .pipe(
+        catchError(e => {
+          if (this.noAutorizado(e)) {
+            return throwError(e);
+          }
+          if (e.status == 400) {
+            return throwError(e);
+          }
+          console.error(e);
+          this.router.navigate(['/login']);
+          console.error(e.error.error_description);
         })
       );
   }
